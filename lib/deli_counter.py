87657ee34f0f24f3bb6de35908397892bef8cf22
Eunice Mwenda <eunice.mwenda@student.moringaschool.com>
2023-10-15 10:41:46 +0300

    deli_counter Lab

---
+++ lib/deli_counter.py
@@ -0,0 +1,21 @@
+katz_deli = []
+
+def take_a_number(line, name):
+    line.append(name)
+    position = len(line)
+    print(f"Welcome, {name}. You are number {position} in line.")
+
+def line(line):
+    if not line:
+        print("The line is currently empty.")
+    else:
+        line_str = "The line is currently: " + " ".join([f"{i + 1}. {name}" for i, name in enumerate(line)])
+        print(line_str)
+
+def now_serving(line):
+    if not line:
+        print("There is nobody waiting to be served!")
+    else:
+        serving_name = line.pop(0)
+        print(f"Currently serving {serving_name}.")
+
